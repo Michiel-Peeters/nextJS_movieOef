d20958a37134fd8758f0c25ab90e2b72e283edee
Michiel Peeters <michiel.peeters@anvil.be>
2022-05-20 16:18:53 +0200

    homepage button

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,14 @@
 import React from "react";
+import Link from "next/link";
 
 const Index = () => {
-  return <div>Home</div>;
+  return (
+    <div className="detail">
+      <Link href={"/films"}>
+        <button>Bekijk de populaire films</button>
+      </Link>
+    </div>
+  );
 };
 
 export default Index;
